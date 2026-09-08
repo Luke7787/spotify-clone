8c93e020947d29c655012a22de8d4484f47b8c64
Luke Zhuang <Lukezhuang@yahoo.com>
2026-09-08 09:57:42 -0700

    set up clerk

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import { Button } from "./components/ui/button";
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
 
 function App() {
   return (
     <>
-      <h1 className="text-red-500 text-5xl">Hello</h1>
-      <Button variant="outline">This is a button</Button>
+      <header>
+        <Show when="signed-out">
+          <SignInButton />
+          <SignUpButton />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
     </>
   );
 }
