abb2be488b02e2d6d4468fdbe8535a18b0c68979
Luke Zhuang <Lukezhuang@yahoo.com>
2026-09-11 07:56:50 -0700

    created .env file in backend folder

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
