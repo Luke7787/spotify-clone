dc989168c89093d3f9570b755d4f6313ba3e130c
Luke Zhuang <Lukezhuang@yahoo.com>
2026-09-12 08:02:29 -0700

    imported routers into index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
